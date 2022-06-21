948e64d12da6a088aca8f019b411bb94f81b701d
pccommen <pccommen@gmail.com>
2022-06-21 21:58:51 +0900

    first commit

---
+++ Projects/FastDevelopeForGyo/Network/Model.swift
@@ -0,0 +1,41 @@
+//
+//  ModelSample.swift
+//  Udong
+//
+//  Created by changgyo seo on 2022/03/04.
+//
+
+import Foundation
+
+struct NetkResponse<T: Codable>: Codable{
+    let success: Bool?
+    var data: T?
+    let error: Bool?
+    
+}
+// MARK: - Encode/decode helpers
+
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+    
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
+}
